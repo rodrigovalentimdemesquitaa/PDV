358c898a77bcb03c73b5672da89c0a91c93ac669
Rodrigo Valentim de Mesquitaa <rodrigovalentimdemesquitaa@gmail.com>
2022-08-02 22:51:45 -0300

    inicio comentado
    iniciando projeto já comentando

---
+++ mynotes.blade.php
@@ -0,0 +1,28 @@
+{{--
+
+*****************************************************************************
+Laravel é um framework php de aplicação web com sintaxe expressiva e elegante
+*****************************************************************************
+
+características
+            > Arquitetura MVC                        > Blade template engine
+            > Comando artisan                        > Cache
+            > Eloquent                               > Autenticação
+            > Fila assíncrona                        > Paginação automática
+            > Database migration e seeding
+*****************************************************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    --}}
