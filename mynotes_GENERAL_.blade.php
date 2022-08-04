11bb7f4426046243208aca9a646ce56b84950a9b
Rodrigo Valentim de Mesquitaa <rodrigovalentimdemesquitaa@gmail.com>
2022-08-04 06:46:39 -0300

    aula 3 rotas comentadas

---
+++ mynotes_GENERAL_.blade.php
@@ -0,0 +1,38 @@
+{{--
+
+-----------------------------------------------------------------------------
+LARAVEL
+    - É um framework php de aplicação web com sintaxe expressiva e elegante.
+-----------------------------------------------------------------------------
+CARACTERÍSTICAS
+    > Arquitetura MVC                        > Blade template engine
+    > Comando artisan                        > Cache
+    > Eloquent                               > Autenticação
+    > Fila assíncrona                        > Paginação automática
+    > Database migration e seeding
+-----------------------------------------------------------------------------
+PASSO A PASSO PARA CRIAR UM NOVO PROJETO LARAVEL
+
+    1. Abrir uma rota
+        1.1 diretório:
+            e-commerce_tm_2\routes\web.php
+
+        1.2 comando:
+            Route::get(uri '/', function(){
+                return view('welcome');
+                })
+------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+    --}}
---
+++ routes/mynotas_ROUTES_.blade.php
@@ -0,0 +1,33 @@
+{{--
+
+------------------------------------------------------------------------------
+ROUTES
+    - URL amigável;
+    - Ao receber uma requisição em uma determinada URL o sistema de rotas
+      define o que fazer, como por exemplo redirecionar ou enviar para o
+      controlador (Controller) decidir o que fazer.
+------------------------------------------------------------------------------
+SINTAXE:
+    Route::get(uri '/', function(){
+        return view('welcome');
+        })
+
+DEFINIÇÃO:
+    - Route == class
+    - get == método
+    - uri: '/' == 1º parâmetro(onde entra o string da rota)
+    - function == 2º parâmetro(entrada da função)
+    - return == saída da função
+
+
+
+
+
+
+
+
+
+
+--}}
+
+
