4b2e034cc65ff8e571a64946cb2a2f134a73c29b
Rodrigo Valentim de Mesquitaa <rodrigovalentimdemesquitaa@gmail.com>
2022-08-04 07:17:26 -0300

    aula 4 rotas(incompleto)
    sintaxe das rotas
    prefix
    group
    nomes nas rotas

--- mynotes_GENERAL_.blade.php
+++ mynotes_GENERAL_.blade.php
@@ -2,36 +2,25 @@
 
 -----------------------------------------------------------------------------
 LARAVEL
-    - É um framework php de aplicação web com sintaxe expressiva e elegante.
+    - é um framework php de aplicação web com sintaxe expressiva e elegante.
 -----------------------------------------------------------------------------
-CARACTERÍSTICAS
-    > Arquitetura MVC                        > Blade template engine
-    > Comando artisan                        > Cache
-    > Eloquent                               > Autenticação
-    > Fila assíncrona                        > Paginação automática
-    > Database migration e seeding
+características
+    > arquitetura MVC                        > blade template engine
+    > comando artisan                        > cache
+    > eloquent                               > autenticação
+    > fila assíncrona                        > paginação automática
+    > database migration e seeding
 -----------------------------------------------------------------------------
-PASSO A PASSO PARA CRIAR UM NOVO PROJETO LARAVEL
-
-    1. Abrir uma rota
+novo projeto laravel
+    1. abrir uma rota
         1.1 diretório:
             e-commerce_tm_2\routes\web.php
 
-        1.2 comando:
+        1.2 comando
             Route::get(uri '/', function(){
                 return view('welcome');
                 })
 ------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
 
 
 
--- routes/mynotas_ROUTES_.blade.php
+++ routes/mynotas_ROUTES_.blade.php
@@ -1,24 +1,40 @@
 {{--
 
-------------------------------------------------------------------------------
+---------------------------------------------------------------------------------
 ROUTES
-    - URL amigável;
-    - Ao receber uma requisição em uma determinada URL o sistema de rotas
+    - url amigável;
+    - ao receber uma requisição em uma determinada URL o sistema de rotas
       define o que fazer, como por exemplo redirecionar ou enviar para o
       controlador (Controller) decidir o que fazer.
-------------------------------------------------------------------------------
-SINTAXE:
+---------------------------------------------------------------------------------
+sintaxe:
     Route::get(uri '/', function(){
         return view('welcome');
         })
 
-DEFINIÇÃO:
+definição:
     - Route == class
-    - get == método
+    - ::get == método
     - uri: '/' == 1º parâmetro(onde entra o string da rota)
-    - function == 2º parâmetro(entrada da função)
+    - function() == 2º parâmetro(entrada da função)
     - return == saída da função
+    - group == método usado quando se tem o mesmo prefixo: 'usuarios', neste caso, para várias url
+        - prefix == prefixo que será repetido
+    - ->name(name: 'usuarios.show) == método para url amigável(nomes nas rotas)
 
+---------------------------------------------------------------------------------
+
+sintaxe
+    Route::prefix(prefix: 'usuarios')->groud(function(){
+
+        Route::get(uri: '/edit', function(){
+            return 'usuarios';
+        })
+        Route::get(uri: '/{id}', function(){
+            return 'Mostrar detalhes';
+        })->name(name: 'usuarios.show);
+    }
+---------------------------------------------------------------------------------
 
 
 
