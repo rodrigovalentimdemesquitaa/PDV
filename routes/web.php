7685220d5f9bc6e55c17733460213e4ddfd44944
Rodrigo Valentim de Mesquitaa <rodrigovalentimdemesquitaa@gmail.com>
2022-08-05 14:36:26 -0300

    Mudanças de planos
    não vou fazer mais esse projeto do laravel 9,
    vou fazer outro usando versão 8

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get(uri: 'user/{user}', function (App\Models\User $user) {
+    return '$user';
 });
