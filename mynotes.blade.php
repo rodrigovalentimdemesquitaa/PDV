8107c96d6a44f162ea00e6ff2e735cb195d54cd4
Rodrigo Valentim de Mesquitaa <rodrigovalentimdemesquitaa@gmail.com>
2022-08-06 13:20:40 -0300

    planejamento
    ideia do que quero fazer no sistema

---
+++ mynotes.blade.php
@@ -0,0 +1,39 @@
+{{--
+
+    Sistema de Caixa para SuperMercado
+        1 login operador(a)
+            1.1 dashboard operador(a)
+                1.1.1 fechar/abrir caixa
+                1.1.2 valor do fundo de caixa
+                1.1.3 valor total de vendas
+                1.1.4 após um valor muito alto no caixa, travar pedindo para fazer sangria(adm)
+
+            1.2 registrar produtos
+                1.1.1 mostrar informações dos produtos individuais num container
+                1.1.2 mostrar todos os produtos noutro container como nota fiscal
+                1.1.3 opção de debido/crédito com taxa a partir de alguma parcela
+                1.1.4 opção de dividir pagamento em cartões/dinheiro
+
+        3 tela visível para cliente dos produtos registrados
+            3.1 mostrar produto indivitual com valor
+            3.2 mostrar valor total das compras
+
+        4 login admistrador
+            4.1 dashboard adminstrador
+                4.1 adicionar/retirar produtos
+                4.2 adicionar/retirar operador(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--}}
