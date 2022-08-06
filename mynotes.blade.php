7daec7e7ce52865b5947df65bb84cb703e530976
Rodrigo Valentim de Mesquitaa <rodrigovalentimdemesquitaa@gmail.com>
2022-08-06 13:26:44 -0300

    correção

--- mynotes.blade.php
+++ mynotes.blade.php
@@ -9,14 +9,14 @@
                 1.1.4 após um valor muito alto no caixa, travar pedindo para fazer sangria(adm)
 
             1.2 registrar produtos
-                1.1.1 mostrar informações dos produtos individuais num container
-                1.1.2 mostrar todos os produtos noutro container como nota fiscal
-                1.1.3 opção de debido/crédito com taxa a partir de alguma parcela
-                1.1.4 opção de dividir pagamento em cartões/dinheiro
+                1.1.1 informações dos produtos individuais num container
+                1.1.2 todos os produtos noutro container como nota fiscal
+                1.1.3 debido/crédito com taxa a partir de alguma parcela
+                1.1.4 dividir pagamento em cartões/dinheiro
 
         3 tela visível para cliente dos produtos registrados
-            3.1 mostrar produto indivitual com valor
-            3.2 mostrar valor total das compras
+            3.1 produto indivitual com valor
+            3.2 valor total das compras
 
         4 login admistrador
             4.1 dashboard adminstrador
