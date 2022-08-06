46e15a02e6137cbf65dc35f8cce3ce9e4d4b38da
Rodrigo Valentim de Mesquitaa <rodrigovalentimdemesquitaa@gmail.com>
2022-08-06 20:26:29 -0300

    esqueleto sendo montado
    iniciei algumas prévias.
    
    controller
    model
    migrate
    views

---
+++ resources/views/operador/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('title', 'Painel de Controle')
+
+@section('content')
+
+    <h1>Aqui vai ficar os controles do adm</h1>
+
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CaixaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get(uri: 'user/{user}', function (App\Models\User $user) {
-    return '$user';
-});
+Route::get('/', [CaixaController::class, 'index']);
+Route::get('/dashboard', [CaixaController::class, 'dashboard']);
+
+
