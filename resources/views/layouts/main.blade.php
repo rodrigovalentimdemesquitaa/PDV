c21bba3720a6427542e4fdc858419e975cc2ccf7
Rodrigo Valentim de Mesquitaa <rodrigovalentimdemesquitaa@gmail.com>
2022-08-07 08:39:15 -0300

    iniciando menu
    botstrap
    ion-icon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,10 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title></head>
+
+   <!-- CSS Bootstrap -->
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+   <!-- Fonte do Google -->
+   <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+
 <body>
 
     <header>
-        cabeçalho
+        <ul class="nav nav-tabs justify-content-center">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Active</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
     </header>
 
     @yield('content')
@@ -16,6 +36,6 @@
     <footer>
         <p>rodapé</p>
     </footer>
-
+    <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
 </body>
 </html>
