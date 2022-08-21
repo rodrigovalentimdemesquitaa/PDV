895400d578c96b5934f24dc6f528d8c745879600
Rodrigo Valentim de Mesquita <rodrigovalentimdemesquitaa@gmail.com>
2022-08-21 19:28:13 -0300

    mudança de projeto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,15 @@
-@extends('layouts.main')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todos os Lembretes</title>
+</head>
+<body>
 
-@section('title', 'PDV')
+    <h1 class="titulo">Lembrete</h1>
+    
 
-@section('content')
-
-
-<h1>Aqui vai ser a página inicial</h1>
-
-
-@endsection
+</body>
+</html>
